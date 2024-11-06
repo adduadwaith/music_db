639ff1b23b82183fa0f92ac7b38a4145aa6cab51
ArchBase <archbase557@gmail.com>
2024-11-06 19:00:14 +0530

    deleted unwanted folders

--- login/script1.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for opening the sidebar from right to left
-function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-}
-
-// JavaScript for closing the sidebar
-function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-}
